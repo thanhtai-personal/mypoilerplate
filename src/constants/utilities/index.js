788f75ab9393ed0fb6a74214080493d981f5bd7b
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-05-24 17:20:06 +0700

    some thing about redux-saga

---
+++ src/constants/utilities/index.js
@@ -0,0 +1,25 @@
+const asyncTypes = {
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR'
+}
+
+export const createAsyncTypes = (typeString) =>
+  (Object.values(asyncTypes).reduce((acc, curr) => {
+    acc[curr] = `${typeString}_${curr}`
+    return acc
+  }, {}))
+
+export const createAction =
+  (type, payload = {}) =>
+    ({ type, ...payload })
+
+///////////////////
+// createReducer //
+///////////////////
+export const createReducer =
+  (initialState, handlers) =>
+    ((state = initialState, action) =>
+      handlers.hasOwnProperty(action.type)
+        ? handlers[action.type](state, action)
+        : state)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,3 +5,29 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+
+/**
+ *
+ export const GET_BLOG_ASYNC = createAsyncTypes('GET_BLOG')
+ switch (action.type) {
+  case types.GET_BLOG_ASYNC.PENDING:
+    return {
+      ...state,
+      loading: true
+    }
+  case types.GET_BLOG_ASYNC.SUCCESS:
+    return {
+      ...state,
+      posts: action.posts,
+      loading: false
+    }
+  case types.GET_BLOG_ASYNC.ERROR:
+    return {
+      ...state,
+      loading: false
+    }
+  default:
+    return state
+}
+ */
